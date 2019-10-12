7eb9d0cf3297cf15f5a3ecb952bb8578fdccc568
Diego Cruz <abeja.dact_@hotmail.com>
2019-10-12 08:42:44 -0500

    hasta clase 9 usando Roputer no funciona login

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,38 +10,35 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initalState'
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
   return initialState.length === 0 ? <h1>Loading...</h1> : (
-    <div className="App">
-      <Header />
+    <>
       <Search />
-      {initialState.mylist.length > 0 &&
-        <Categories title="Mi Lista">
+      {initialState.mylist.length > 0 && (
+        <Categories title='Mi Lista'>
           <Carousel>
             {initialState.mylist.map(item =>
-              <CarouselItem key={item.id} {...item} />
-            )}
+              <CarouselItem key={item.id} {...item} />)}
           </Carousel>
         </Categories>
-      }
-      <Categories title="Tendencias">
+      )}
+      <Categories title='Tendencias'>
         <Carousel>
           {initialState.trends.map(item =>
             <CarouselItem key={item.id} {...item} />
           )}
         </Carousel>
       </Categories>
-      <Categories title="Originales de Platzi Video">
+      <Categories title='Originales de Platzi Video'>
         <Carousel>
           {initialState.originals.map(item =>
             <CarouselItem key={item.id} {...item} />
           )}
         </Carousel>
       </Categories>
-      <Footer />
-    </div>
+    </>
   );
-}
+};
 
-export default App;
+export default Home;
